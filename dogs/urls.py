5c2aff81b1f90debf62401f8b767f3f666cc8206
Salman Abdulla <alhashimxpo2@gmail.com>
2025-09-17 14:02:31 +0300

    Fixing ui/ux errors

--- dogs/urls.py
+++ dogs/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('<int:pk>/delete/', views.DogDeleteView.as_view(), name='delete'),
     path('<int:pk>/favorite/', views.toggle_favorite, name='toggle_favorite'),
     path('<int:pk>/order/', views.create_order, name='create_order'),
+    path('order/<int:order_id>/accept/', views.accept_order, name='accept_order'),
+    path('order/<int:order_id>/decline/', views.decline_order, name='decline_order'),
+    path('order/<int:order_id>/cancel/', views.cancel_order, name='cancel_order'),
+    path('order/<int:order_id>/complete/', views.complete_order, name='complete_order'),
 ]
