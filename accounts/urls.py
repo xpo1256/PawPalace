1c9ca71b317947061d0ef6d3ff5469d1486ea978
Salman Abdulla <alhashimxpo2@gmail.com>
2025-09-17 14:08:05 +0300

    Add forgetpassword features

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
@@ -13,4 +14,21 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('seller-orders/', views.seller_orders, name='seller_orders'),
     path('seller/<int:pk>/', views.seller_profile, name='seller_profile'),
+    # Password reset
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset_form.html',
+        email_template_name='accounts/password_reset_email.html',
+        subject_template_name='accounts/password_reset_subject.txt',
+        success_url= '/accounts/password-reset/done/'
+    ), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html',
+        success_url='/accounts/reset/done/'
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
