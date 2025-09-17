c0fecb1a94715e9faaa08b08d8fb0210121123fd
Salman Abdulla <alhashimxpo2@gmail.com>
2025-09-17 14:09:16 +0300

    Add missing things

--- dog_marketplace/dog_marketplace/settings.py
+++ dog_marketplace/dog_marketplace/settings.py
@@ -155,6 +155,9 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Email (development)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
